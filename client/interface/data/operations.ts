46af930235cffa69730dc10f2ecd22178d87016b
Alex Badham <awbadham@gmail.com>
2019-11-09 21:21:17 -0800

    added production build envs for heroku so the react can connect to the heroku backend

--- client/interface/data/operations.ts
+++ client/interface/data/operations.ts
@@ -3,22 +3,24 @@ import axios, { AxiosResponse } from 'axios';
 import { Dispatch } from 'react';
 import { Data } from './interface';
 
+const server = process.env.NODE_ENV === 'production' ? 'https://story-picker.herokuapp.com' : 'http://localhost:3000';
+
 const loadStoryTypes = () => (dispatch: Dispatch<any>) => {
-    axios.get('http://localhost:3000/data/story-types')
+    axios.get(server + '/data/story-types')
         .then((response: AxiosResponse<Data[]>) => {
             dispatch(actions.loadStoryTypes(response.data));
         });
 }
 
 const loadSettings = () => (dispatch: Dispatch<any>) => {
-    axios.get('http://localhost:3000/data/settings')
+    axios.get(server + '/data/settings')
         .then((response: AxiosResponse<Data[]>) => {
             dispatch(actions.loadSettings(response.data));
         });
 }
 
 const loadPlotPoints = () => (dispatch: Dispatch<any>) => {
-    axios.get('http://localhost:3000/data/plot-points')
+    axios.get(server + '/data/plot-points')
         .then((response: AxiosResponse<Data[]>) => {
             dispatch(actions.loadPlotPoints(response.data));
         });
